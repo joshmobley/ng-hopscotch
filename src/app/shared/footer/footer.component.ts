27240a16d31af5eb6feb5923bdc6b3f081fecfd3
Josh Mobley <josh@joshmobley.net>
2017-04-26 01:04:59 -0400

    updates to footer + single, also favicon and 200

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,9 +6,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  randomInt: number;
+  quotes: [string] = [
+    'The energy, passion, and enthusiasm that I witnessed, has now been rekindled in me.',
+    'A year’s worth of inspiration in one delicious, social nugget.',
+    'A meeting of creative minds and local professionals changing the meaning of design.',
+    'Hopscotch Design Festival has a gritty, design-focused, innovative edge that other conferences just don’t have.'
+  ];
+
   constructor() { }
 
+  returnImage() {
+    let styles = {};
+    let i = Math.floor(Math.random()*7) + 1;
+    styles['background-image'] = 'url(http://hopdesign.wpengine.com/wp-content/uploads/2017/04/Footer_hero_image_' + i + '-1000x667.jpg)';
+    return styles;
+
+  }
+
   ngOnInit() {
+    this.randomInt = Math.floor(Math.random() * 3);
   }
 
 }
